6bf8b9784020005f179cfdc6d28541f78fbcc1b3
Steve Cook <stevecook23@gmail.com>
2024-08-16 12:23:45 +0100

    Added dotenv to requirements.txt, and basic homepage view

--- foodshop/urls.py
+++ foodshop/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', views.home, name='home'),
 ]
