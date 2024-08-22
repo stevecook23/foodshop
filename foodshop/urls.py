46c2d3012e5705f783abdd9709169ded7924793f
Steve Cook <stevecook23@gmail.com>
2024-08-22 11:10:38 +0100

    Images now work

--- foodshop/urls.py
+++ foodshop/urls.py
@@ -26,3 +26,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
