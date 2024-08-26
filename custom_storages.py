23afbf5906e3a5eda2ee5d9ddb35923e522aa9c5
Steve Cook <stevecook23@gmail.com>
2024-08-26 20:59:18 +0100

    Adding logging to custom_storages

--- custom_storages.py
+++ custom_storages.py
@@ -8,3 +8,18 @@ class StaticStorage(S3Boto3Storage):
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+    
+
+import logging
+logger = logging.getLogger(__name__)
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+    
+def __init__(self, *args, **kwargs):
+    logger.info(f"Initializing StaticStorage with location: {self.location}")
+    super().__init__(*args, **kwargs)
+
+def save(self, name, content, max_length=None):
+    logger.info(f"Attempting to save {name} to S3")
+    return super().save(name, content, max_length)
