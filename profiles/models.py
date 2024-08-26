9ed605b5740b0f3832918e2dc37cc98bc58ff02d
Steve Cook <stevecook23@gmail.com>
2024-08-26 17:31:56 +0100

    Squashing bug - can't login as admin

--- profiles/models.py
+++ profiles/models.py
@@ -29,7 +29,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
-    if created:
+    if not hasattr(instance, 'userprofile'):
         UserProfile.objects.create(user=instance)
-    # Existing users: just save the profile
     instance.userprofile.save()
