19fabb40f686ea115b81c3d1ea3032dd0588aec1
Steve Cook <stevecook23@gmail.com>
2024-08-26 19:15:14 +0100

    CSS fixes for login, logout, register

--- foodshop/urls.py
+++ foodshop/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profiles/', include('profiles.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
