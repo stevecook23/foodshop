a08966e3da257d14d6aa09b426bc75d5e413c14b
Steve Cook <stevecook23@gmail.com>
2024-08-27 16:16:32 +0100

    Debugging a server error 500 from Heroku

--- foodshop/settings.py
+++ foodshop/settings.py
@@ -239,3 +239,19 @@ STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET', '')
 
 
 DEBUG = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    },
+}
