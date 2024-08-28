80dd316e7c92f96b861ce3f92ee06d0ed3811b67
Steve Cook <stevecook23@gmail.com>
2024-08-28 15:13:15 +0100

    Further changes to overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -46,79 +46,81 @@ card.addEventListener('change', function (event) {
 // Handle form submit
 var form = document.getElementById('payment-form');
 
-form.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-    card.update({ 'disabled': true });
-    $('#submit-button').attr('disabled', true);
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById('payment-form');
+    form.addEventListener('submit', function(ev) {
+        ev.preventDefault();
 
-    // Hide the form immediately
-    $('#payment-form').css('visibility', 'hidden');
-    
-    // Show the overlay immediately
-    $('#loading-overlay').css('display', 'block');
+        card.update({ 'disabled': true });
+        $('#submit-button').attr('disabled', true);
 
-    var saveInfo = Boolean($('#id-save-info').attr('checked'));
-    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-    var postData = {
-        'csrfmiddlewaretoken': csrfToken,
-        'client_secret': clientSecret,
-        'save_info': saveInfo,
-    };
-    var url = '/checkout/cache_checkout_data/';
+        // Ensure the form is hidden and overlay is shown only on submission
+        $('#payment-form').css('visibility', 'hidden');
+        $('#loading-overlay').css('display', 'block');
 
-    $.post(url, postData).done(function () {
-        stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: card,
-                billing_details: {
+        var saveInfo = Boolean($('#id-save-info').attr('checked'));
+        var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+        var postData = {
+            'csrfmiddlewaretoken': csrfToken,
+            'client_secret': clientSecret,
+            'save_info': saveInfo,
+        };
+        var url = '/checkout/cache_checkout_data/';
+
+        $.post(url, postData).done(function () {
+            stripe.confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: $.trim(form.full_name.value),
+                        phone: $.trim(form.phone_number.value),
+                        email: $.trim(form.email.value),
+                        address:{
+                            line1: $.trim(form.street_address1.value),
+                            line2: $.trim(form.street_address2.value),
+                            city: $.trim(form.town_or_city.value),
+                            country: $.trim(form.country.value),
+                            state: $.trim(form.county.value),
+                        }
+                    }
+                },
+                shipping: {
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
-                    email: $.trim(form.email.value),
-                    address:{
+                    address: {
                         line1: $.trim(form.street_address1.value),
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
+                        postal_code: $.trim(form.postcode.value),
                         state: $.trim(form.county.value),
                     }
-                }
-            },
-            shipping: {
-                name: $.trim(form.full_name.value),
-                phone: $.trim(form.phone_number.value),
-                address: {
-                    line1: $.trim(form.street_address1.value),
-                    line2: $.trim(form.street_address2.value),
-                    city: $.trim(form.town_or_city.value),
-                    country: $.trim(form.country.value),
-                    postal_code: $.trim(form.postcode.value),
-                    state: $.trim(form.county.value),
-                }
-            },
-        }).then(function(result) {
-            if (result.error) {
-                var errorDiv = document.getElementById('card-errors');
-                var html = `
-                    <span class="icon" role="alert">
-                    <i class="bi bi-x-circle"></i>
-                    </span>
-                    <span>${result.error.message}</span>`;
-                $(errorDiv).html(html);
+                },
+            }).then(function(result) {
+                if (result.error) {
+                    var errorDiv = document.getElementById('card-errors');
+                    var html = `
+                        <span class="icon" role="alert">
+                            <i class="bi bi-x-circle"></i>
+                        </span>
+                        <span>${result.error.message}</span>`;
+                    $(errorDiv).html(html);
 
-                // Restore the form and hide the overlay
-                $('#payment-form').css('visibility', 'visible');
-                $('#loading-overlay').css('display', 'none');
-                
-                card.update({ 'disabled': false });
-                $('#submit-button').attr('disabled', false);
-            } else {
-                if (result.paymentIntent.status === 'succeeded') {
-                    form.submit();
+                    // Restore the form and hide the overlay if there's an error
+                    $('#payment-form').css('visibility', 'visible');
+                    $('#loading-overlay').css('display', 'none');
+                    
+                    card.update({ 'disabled': false });
+                    $('#submit-button').attr('disabled', false);
+                } else {
+                    if (result.paymentIntent.status === 'succeeded') {
+                        form.submit();
+                    }
                 }
-            }
+            });
+        }).fail(function () {
+            // Reload the page, the error will be in Django messages
+            location.reload();
         });
-    }).fail(function () {
-        // Reload the page, the error will be in Django messages
-        location.reload();
-    })
+    });
 });
