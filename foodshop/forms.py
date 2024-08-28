a9accbba00710b91f697aeafa62a80ce8015a6e8
Steve Cook <stevecook23@gmail.com>
2024-08-28 13:19:37 +0100

    Edit to forms.py for signup form

--- foodshop/forms.py
+++ foodshop/forms.py
@@ -5,4 +5,5 @@ class CustomSignupForm(SignupForm):
         super(CustomSignupForm, self).__init__(*args, **kwargs)
         for fieldname, field in self.fields.items():
             field.widget.attrs['placeholder'] = ''
+            field.widget.attrs['class'] = 'form-control'
             field.label = field.label.capitalize()  # Ensure labels are capitalized
