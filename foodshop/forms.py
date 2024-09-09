09a2d01726d95fc2cd82b6f252117de12b791836
Steve Cook <stevecook23@gmail.com>
2024-09-09 11:18:43 +0100

    User feedback - improved messaging on signup page

--- foodshop/forms.py
+++ foodshop/forms.py
@@ -1,4 +1,8 @@
+"""Forms for the custom signup and login pages."""
 from allauth.account.forms import SignupForm, LoginForm
+from django import forms
+from django.contrib.auth import get_user_model
+
 
 class CustomSignupForm(SignupForm):
     def __init__(self, *args, **kwargs):
@@ -7,7 +11,14 @@ class CustomSignupForm(SignupForm):
             field.widget.attrs['placeholder'] = ''
             field.widget.attrs['class'] = 'form-control'
             field.label = field.label.capitalize()
-            
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        User = get_user_model()
+        if User.objects.filter(username__iexact=username).exists():
+            raise forms.ValidationError("A user with this username already exists.")
+        return username
+
 class CustomLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
         super(CustomLoginForm, self).__init__(*args, **kwargs)
