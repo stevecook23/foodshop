d163ba9c3303526bae000f3b694b9b1be00c74cf
Steve Cook <stevecook23@gmail.com>
2024-09-16 14:34:59 +0100

    Debug off

--- foodshop/settings.py
+++ foodshop/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['foodshop-5f1ae0a958b9.herokuapp.com', 
                  '127.0.0.1', 'localhost',]
