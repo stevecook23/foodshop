940292f50c0f9e1ecf2c0bfec36b0d72ad88f7b4
Steve Cook <stevecook23@gmail.com>
2024-09-16 13:51:47 +0100

    Minor changes to tidy up code, remove redundancies

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -130,14 +130,12 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                product=product,
-                                quantity=quantity,
-                                product_size=size,
-                            )
-                            order_line_item.save()
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,                            
+                        )
+                        order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
